5db2421e3c917cfaf2bb23de6a57cd60c42a1858
lain <lain@champagne.connected>
2024-11-05 16:12:18 +0000

    working depth first fs scan with checks for directories

--- script.py
+++ script.py
@@ -1,6 +1,5 @@
-#1. scan fs -> how?
+#DONE. scan fs -> how?
 # go from root and go through each file, breadth or depth first, and hash it
-# compare hash to hashes in look up table 
 
 
 
@@ -8,3 +7,42 @@
 #3. upload remaining files to Virustotal via api
 
 #4. VM (optional)
+
+#break this up:
+#first scan directory
+#then directory recursively for other directories
+#try it from root dir
+
+import os, hashlib
+directory = "/home/lain/school/appsec/malicious-file-detection-tool"
+def hash_file(file):
+    with open(file,"rb") as file:
+        digest = hashlib.file_digest(file, "sha1")
+        return digest.hexdigest()
+
+#def compare_hash(file):
+#    if hash_file(file)
+
+
+
+def scan_directory(root_dir):
+    stack = [root_dir]
+    while stack:
+        current_dir = stack.pop()
+        try:
+            with os.scandir(current_dir) as entries:
+                for entry in entries:
+                    if entry.is_dir():
+                        stack.append(entry.path)
+                    else:
+                        print(hash_file(entry))
+        except PermissionError:
+            continue
+
+
+
+
+scan_directory(directory)
+
+
+
