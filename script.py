1e3f4a2276222afaba07c557bbb3cfb44e8a828a
otitoko <tbkopera@proton.me>
2024-11-05 17:14:31 +0100

    explanation

---
+++ script.py
@@ -0,0 +1,10 @@
+#1. scan fs -> how?
+# go from root and go through each file, breadth or depth first, and hash it
+# compare hash to hashes in look up table 
+
+
+
+#2. use look up database, eg. NIST NSRL
+#3. upload remaining files to Virustotal via api
+
+#4. VM (optional)
